a8a6870f972d6998a36846995b32e5c24dc1bce2
Mendivelso <Mendivelso@Mendivelso-PC>
2019-09-27 17:29:03 -0500

    Another

--- PruebaEntrega/src/Prototipo1.java
+++ PruebaEntrega/src/Prototipo1.java
@@ -10,6 +10,8 @@ public class Prototipo1 {
 		lista_estudiantes.PushBack(e1);
 		lista_estudiantes.PushBack(e1);
 		lista_estudiantes.PushBack(e1);
+		lista_estudiantes.PushBack(e1);
+		//Hola
 		lista_estudiantes.PushFront(new Estudiante(69,"Pepe","Pepin",new GregorianCalendar(2000,01,01)));
 		lista_estudiantes.DisplayList();
 	}
